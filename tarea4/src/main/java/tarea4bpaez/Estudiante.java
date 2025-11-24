c6795fafbc34220fbd66a72e834785185b266a75
Alejandro <apaemil0312@g.educaand.es>
2025-11-24 19:44:43 +0100

    termine

--- tarea4/src/main/java/tarea4bpaez/Estudiante.java
+++ tarea4/src/main/java/tarea4bpaez/Estudiante.java
@@ -1,41 +1,60 @@
 package tarea4bpaez;
 
-import java.lang.reflect.Constructor;
+
 
 public class Estudiante {
+    // Atributos privados
+    private String nombre;
+    private int idEstudiante;
+    private double notaMedia;
+    private String curso;
 
-       // Atributos Privados.
-        private String nombre;
-        private int idEstudiante;
-        private double notaMedia;
-        private String curso;
+    // Constructor
+    public Estudiante(String nombre, int idEstudiante, String curso) {
+        this.nombre = nombre;
+        this.idEstudiante = idEstudiante;
+        this.curso = curso;
+        this.notaMedia = 0.0; // Inicialización a 0.0
+    }
 
-        // Constructor (Inicializa loa atributos privados)
-        public Estudiante (String nombre, int idEstudiante, String curso ){
-            this.nombre = nombre;
-            this.idEstudiante = idEstudiante;
-            this.curso = curso;
-            this.notaMedia = 0.0;
-        }  
+    // Métodos get
+    public String getNombre() {
+        return nombre;
+    }
 
-        // METODOS GET 
+    public int getIdEstudiante() {
+        return idEstudiante;
+    }
 
-        public String getNombre(){
-            return nombre;
+    public double getNotaMedia() {
+        return notaMedia;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    // Métodos set
+    public void setNotaMedia(double notaMedia) {
+        if (notaMedia >= 0.0 && notaMedia <= 10.0) {
+            this.notaMedia = notaMedia;
+        } else {
+            System.out.println("Error: La nota media debe estar entre 0.0 y 10.0.");
         }
-        public int getidEstudiante(){
-            return idEstudiante;
-        }
-        public double getnotaMedia(){
-            return notaMedia;
-        }
-        public String getsurso(){
-            return curso;
-        }
-        
-        // METODS SET
+    }
 
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
 
+    // Sobreescribir toString()
+    @Override
+    public String toString() {
+        return "Estudiante: " + nombre + " (ID: " + idEstudiante + "), Curso: " + curso + ", Nota Media: " + notaMedia;
+    }
 
-    
+    // Método aprobado
+    public boolean aprobado() {
+        return notaMedia >= 5.0;
+    }
 }
--- tarea4/src/main/java/tarea4bpaez/Main.java
+++ tarea4/src/main/java/tarea4bpaez/Main.java
@@ -2,6 +2,22 @@ package tarea4bpaez;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // Crear objetos de Estudiante
+        Estudiante estudiante1 = new Estudiante("Ricardo Dominguez", 101, "1DAW");
+        Estudiante estudiante2 = new Estudiante("Ingris ", 102, "1DAW");
+
+        // Establecer notas
+        estudiante1.setNotaMedia(10.0);
+        estudiante2.setNotaMedia(0.1);
+
+        // Imprimir información de los estudiantes
+        System.out.println(estudiante1);
+        System.out.println(estudiante2);
+
+        // Comprobar si los estudiantes aprobaron
+        System.out.println(estudiante1.getNombre() + " aprobado: " + estudiante1.aprobado());
+        System.out.println(estudiante2.getNombre() + " aprobado: " + estudiante2.aprobado());
+        
     }
 }
